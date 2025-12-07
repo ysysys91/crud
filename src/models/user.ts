c5c3652f1b6798d7585872e3f3e8e02b71537c7e
ysysys91 <ys0414choiys14@gmail.com>
2025-12-07 20:25:40 +0900

    f

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,15 +1,19 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose, { Schema, models } from 'mongoose'
 
 interface IUser {
   name: string
   email: string
 }
 
-const userSchema = new Schema<IUser>({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-})
+const UserSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+)
 
-const User = mongoose.models.User || mongoose.model('User', userSchema)
+// 🔥 중요: models.User 체크할 때 models가 undefined일 수도 있기 때문에 보호 필요
+const User = models?.User || mongoose.model('User', UserSchema)
 
 export default User
